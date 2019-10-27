5872a2d7b8359575bfcd203d3e4238cfe9ba2501
GoranNikic <goran@novnik.com>
2019-10-27 09:24:25 +0000

    Added Joi for validation

---
+++ src/config.js
@@ -0,0 +1,40 @@
+
+
+import Joi from "@hapi/joi";
+
+const internals = {};
+
+exports.apply = function (type, options, ...message) {
+
+  const result = internals[ type ].validate(options);
+
+  if (result.error) {
+    throw new Error(`Invalid ${type} options ${message.length ? `(${ message.join(" ") })` : ""} ${result.error.annotate()}`);
+  }
+
+  return result.value;
+};
+
+internals.cachePolicy = Joi.object({
+  "cache": Joi.string().allow(null).allow(""),
+  "segment": Joi.string(),
+  "shared": Joi.boolean()
+}).unknown();
+
+internals.method = Joi.object({
+  "bind": Joi.object().allow(null),
+  "generateKey": Joi.function(),
+  "cache": internals.cachePolicy
+});
+
+internals.methodObject = Joi.object({
+  "name": Joi.string().required(),
+  "method": Joi.function().required(),
+  "options": Joi.object()
+});
+
+internals.linkObject = Joi.object({
+  "name": Joi.string().required(),
+  "options": Joi.object().optional()
+
+});
