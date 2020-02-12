1f05058b7d53c3dee03b39fa574be5d2497db181
GoranNikic <goran@novnik.com>
2020-02-12 02:33:13 +0100

    Explorer namespaced loggers + additional node loggers

--- src/components/explorer.js
+++ src/components/explorer.js
@@ -26,27 +26,39 @@ export default class Explorer extends Explore {
     this.advertise(this.advertisement);
 
     this.me.id = this.broadcast.instanceUuid;
+
     this.me.processId = this.broadcast.processUuid;
+
     this.me.processCommand = process.argv.slice(1).map((n) => {
       return n.split("/").slice(-2).join("/");
     }).join(" ");
 
     options.log && this.log(this.helloLogger());
 
-    this.on("added", (obj) => {
-      if (!options.monitor && obj.advertisement.key != this.advertisement.key) {
-        return;
-      }
+    this.on("promotion", () =>  {
+      this.log(["Process promoted to master node"]);
+    });
 
-      options.log && options.statusLogsEnabled && options.helloLogsEnabled && this.log(this.statusLogger(obj, "online"));
+    this.on("demotion", () => {
+      this.log(["Process demoted from master"]);
+    });
+
+    this.on("master", () => {
+      this.log(["Process allocated to master node"]);
+    });
+
+    this.on("added", (obj) => {
+        if (!options.monitor && obj.advertisement.key !== this.advertisement.key || obj.advertisement.namespace !== this.advertisement.namespace) {
+          return;
+        }
+      this.log && options.statusLogsEnabled && options.helloLogsEnabled && this.log(this.statusLogger(obj, "online"));
     });
 
     this.on("removed", (obj) => {
-      if (!options.monitor && obj.advertisement.key != this.advertisement.key) {
+      if (!options.monitor && obj.advertisement.key !== this.advertisement.key || obj.advertisement.namespace !== this.advertisement.namespace) {
         return;
       }
-
-      options.log && options.statusLogsEnabled && this.log(this.statusLogger(obj, "offline"));
+      this.log && options.statusLogsEnabled && this.log(this.statusLogger(obj, "offline"));
     });
   }
 
@@ -63,18 +75,17 @@ export default class Explorer extends Explore {
   }
 
   statusLogger(obj, status) {
+
     const logs = [];
 
     if (status) {
-      const statusLog = status == "online" ? ".online".green : ".offline".red;
+      const statusLog = status === "online" ? ".online".green : ".offline".red;
 
-      logs.push(this.advertisement.name, ">", obj.advertisement.type.magenta + statusLog);
-    } else {
-      logs.push();
+      logs.push([this.advertisement.namespace], [this.advertisement.name], "=>", obj.advertisement.type.magenta + statusLog + " --");
     }
 
     logs.push(
-      `${obj.advertisement.name.white}${"#".grey}${obj.id.grey}`);
+      `[ ${obj.advertisement.namespace} ] - ${obj.advertisement.name.white}${"#".grey}${obj.id.grey}`);
 
     if (obj.advertisement.port) {
       logs.push("on", obj.advertisement.port.toString().blue);
